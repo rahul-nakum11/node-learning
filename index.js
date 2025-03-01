2f6c07965b8b547a4e7b622417a798477eedb0fe
Rahul Nakum <nakumrahul5@gmail.com>
2025-03-01 15:21:08 +0530

    learn how URL module works

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const fs = require("fs");
 const http = require("http");
-
+const url = require("url");
 const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()} : ${req.url} New requiest received\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("logger.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("Home Page");
         break;
