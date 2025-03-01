bf091f6eb4337debf28bb73ea2239c1833ef73b0
Rahul Nakum <nakumrahul5@gmail.com>
2025-03-01 15:06:07 +0530

    learn how to create a server on node and create log of incomming request

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const Math = require("./Math");
+const fs = require("fs");
 const http = require("http");
 
-console.log(Math.add(8, 566));
+const myServer = http.createServer((req, res) => {
+  const log = `${Date.now()} : ${req.url} New requiest received\n`;
+  fs.appendFile("logger.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("Home Page");
+        break;
+      case "/about":
+        res.end("About Page");
+        break;
+      case "/contact":
+        res.end("Contact Page");
+        break;
+      default:
+        res.end("404 Not FOund");
+        break;
+    }
+  });
+});
+
+myServer.listen(8000, () => console.log("server started!"));
